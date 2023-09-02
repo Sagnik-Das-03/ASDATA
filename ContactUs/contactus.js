f38c95fb530039297640cd8199da4e0ed1d97015
Sagnik-Das-03 <sagnik07das@gmail.com>
2023-09-02 16:27:31 +0530

    added icons

--- ContactUs/contactus.js
+++ ContactUs/contactus.js
@@ -1,33 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector(".glass-form");
-    const submitButton = document.getElementById("submit-button");
-  
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      const name = document.getElementById("name").value;
-      const email = document.getElementById("email").value;
-      const message = document.getElementById("message").value;
-  
-      if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
-        alert("Please fill in all fields");
-        return;
-      }
-  
-      if (!isValidEmail(email)) {
-        alert("Please enter a valid email address");
-        return;
-      }
-  
-      // If all validations pass, you can submit the form to a server or take other actions
-      alert("Form submitted successfully!");
-      form.reset();
-    });
-  
-    function isValidEmail(email) {
-      // A basic email validation regex pattern
-      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return emailPattern.test(email);
+  const form = document.querySelector(".glass-form");
+  const submitButton = document.getElementById("submit-button");
+  const popup = document.getElementById("popup");
+  const popupMessage = document.getElementById("popup-message");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
+      alert("Please fill in all fields");
+      return;
     }
+
+    if (!isValidEmail(email)) {
+      alert("Please enter a valid email address");
+      return;
+    }
+
+    // If all validations pass, you can submit the form to a server or take other actions
+    popupMessage.textContent = "Form submitted successfully!";
+    popup.style.display = "block";
+    form.reset();
+
+    // Hide the pop-up message after a few seconds (e.g., 3 seconds)
+    setTimeout(function () {
+      popup.style.display = "none";
+    }, 3000);
   });
-  
+
+  function isValidEmail(email) {
+    // A basic email validation regex pattern
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+});
--- News/news.js
+++ News/news.js
@@ -1,19 +1,55 @@
-function toggleNews(sectionId) {
-  const newsList = document.getElementById(sectionId);
-  if (newsList.style.display === "none") {
-      newsList.style.display = "block";
-  } else {
-      newsList.style.display = "none";
-  }
+// Define news items as arrays
+const collegeNews = [
+    "College News 1",
+    "College News 2",
+    "College News 3"
+];
+
+const techNews = [
+    "Local Tech News 1",
+    "Local Tech News 2",
+    "Local Tech News 3"
+];
+
+const clubNews = [
+    "Club News 1",
+    "Club News 2",
+    "Club News 3"
+];
+
+// Function to populate the news lists
+function populateNewsList(newsArray, listId) {
+    const newsList = document.getElementById(listId);
+    newsArray.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        newsList.appendChild(li);
+    });
 }
 
-function toggleAllNews() {
-  const sections = document.querySelectorAll('.news-list');
-  sections.forEach((section) => {
-      if (section.style.display === "none") {
-          section.style.display = "block";
-      } else {
-          section.style.display = "none";
-      }
-  });
+function toggleNews(sectionId) {
+    const newsList = document.getElementById(sectionId);
+    if (newsList.style.display === "none" || newsList.style.display === "") {
+        newsList.style.display = "block";
+    } else {
+        newsList.style.display = "none";
+    }
 }
+
+
+function toggleAllNews() {
+    const sections = document.querySelectorAll('.news-list');
+    sections.forEach((section) => {
+        if (section.style.display === "none") {
+            section.style.display = "block";
+        } else {
+            section.style.display = "none";
+        }
+    });
+}
+
+
+// Call the populateNewsList function for each section
+populateNewsList(collegeNews, "college");
+populateNewsList(techNews, "tech");
+populateNewsList(clubNews, "club");
