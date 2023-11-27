7975d3df873f8824fa1a0af3e9ffe6e179ef8403
Sagnik-Das-03 <sagnik07das@gmail.com>
2023-11-27 19:53:23 +0530

    Added Content

--- AboutUs/aboutus.js
+++ AboutUs/aboutus.js
@@ -1,10 +1,10 @@
 const teamMembers = [
-    { name: 'Member 1', role: 'Role 1' },
-    { name: 'Member 2', role: 'Role 2' },
-    { name: 'Member 3', role: 'Role 3' },
-    { name: 'Member 4', role: 'Role 4' },
-    { name: 'Member 5', role: 'Role 5' },
-    { name: 'Member 6', role: 'Role 6' }
+    { name: 'Ankita Bandyopadhyay', role: '' },
+    { name: 'Sagnik Das', role: '' },
+    { name: 'Debarun Das', role: '' },
+    { name: 'Arkapriya Sinha', role: '' },
+    { name: 'Trisha Kabiraj', role: '' },
+    { name: 'Arghyadeep Karmakar', role: '' }
 ];
 
 const teamMembersContainer = document.querySelector('.team-members');
